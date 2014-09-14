b8d831acf3932439f61f4b9eb1de482db2a55b69
Christian <limbergorama@gmail.com>
2014-09-14 14:11:15 +0200

    update threads and switched to netbeans

---
+++ trunk/BluetoothLogger/src/eu/limbergorama/btlogger/LogWriter.java
@@ -0,0 +1,117 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eu.limbergorama.btlogger;
+
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.widget.TextView;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author chris
+ */
+public class LogWriter implements Runnable{
+    private final BluetoothDevice btDevice;
+        private static final String UUID_SERIAL_PORT_PROFILE
+            = "00001101-0000-1000-8000-00805F9B34FB";
+    private final String filePath;
+        private final String fileName;
+     FileWriter wr;
+     BufferedReader mBufferedReader;
+    private MainActivity observer;
+
+    public LogWriter(String filepath, String filename, BluetoothDevice btDev) {
+        
+        
+        
+        this.btDevice = btDev;
+        this.filePath = filepath;
+        this.fileName = filename;
+        this.initBt();
+        this.initFw();
+        this.observer = null;
+        
+    }
+    
+    private void initBt() {
+                               if (btDevice.getBondState() == BluetoothDevice.BOND_NONE) {
+                            btDevice.createBond();
+                            //info.append("bondet device " + btd.getName() + "\n");
+                        }
+                        //info.append(btd.toString());
+
+                        BluetoothSocket mSocket = null;
+                        mBufferedReader = null;
+                        InputStream aStream = null;
+                        InputStreamReader aReader = null;
+                        try {
+                            mSocket = btDevice.createRfcommSocketToServiceRecord(UUID.fromString(UUID_SERIAL_PORT_PROFILE));
+                            mSocket.connect();
+                            aStream = mSocket.getInputStream();
+
+                        } catch (IOException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+
+                        aReader = new InputStreamReader(aStream);
+                        mBufferedReader = new BufferedReader(aReader);
+
+        
+    }
+    private void initFw() {
+        File fi = new File(this.filePath);
+        if(!fi.exists()) fi.mkdirs();
+        
+        
+        
+        File fifi = new File(this.filePath+"/"+fileName);
+        if(fifi.exists()) fifi.delete();
+        
+        
+        
+        try {
+            wr = new FileWriter(fifi);
+        } catch (IOException ex) {
+            Logger.getLogger(LogWriter.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    public void run() {
+       
+
+                        String line = "";
+                        try {
+                            while ((line = mBufferedReader.readLine()) != null) {
+                                wr.write(line + "\n");
+                                wr.flush();
+                                try {
+                                if(this.observer != null) observer.printInfo(line);
+                                }
+                                catch (Exception ex) {}
+                               }
+                            wr.close();
+                        } catch (IOException e) {
+                            // TODO Auto-generated catch block
+                            e.printStackTrace();
+                        }
+    }
+    
+    public void enableObserver(MainActivity tw) {
+        this.observer = tw;
+    }
+
+
+    
+}
---
+++ trunk/BluetoothLogger/src/eu/limbergorama/btlogger/MainActivity.java
@@ -0,0 +1,185 @@
+package eu.limbergorama.btlogger;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.nio.CharBuffer;
+import java.util.Set;
+import java.util.UUID;
+
+import android.app.Activity;
+import android.app.Application;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import static android.widget.AbsListView.CHOICE_MODE_SINGLE;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends Activity {
+
+    private Context context;
+    private final static int REQUEST_ENABLE_BT = 1337;
+
+
+    ArrayList<LogWriter> logWriter;
+
+    TextView info;
+    ListView storageListView;
+    EditText fileNameEditText;
+
+    public MainActivity() {
+
+    }
+
+    public boolean cardIsMounted() {
+        String state = Environment.getExternalStorageState();
+
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    synchronized void printInfo(String str)
+    {
+        this.info.setText(this.info.getText()+ "\n" + str);
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        this.context = this;
+
+        this.info = (TextView) findViewById(R.id.infoWindow);
+        this.storageListView = (ListView) findViewById(R.id.storageDeviceList);
+        this.fileNameEditText = (EditText) findViewById(R.id.fileNameEditText);
+
+        logWriter = new ArrayList<LogWriter>();
+
+        List<StorageUtils.StorageInfo> storageList = StorageUtils.getStorageList();
+        ArrayAdapter<StorageUtils.StorageInfo> storageAdapter = new ArrayAdapter<StorageUtils.StorageInfo>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
+        storageAdapter.addAll(storageList);
+
+        storageListView.setChoiceMode(CHOICE_MODE_SINGLE);
+        storageListView.setAdapter(storageAdapter);
+
+        if (this.cardIsMounted()) {
+            printInfo("External Storage Device was found");
+
+            File newF = new File(Environment.getExternalStorageDirectory(), "BluetoothOutput.txt");
+
+            try {
+                final FileWriter wr = new FileWriter(newF);
+
+                BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+                if (mBluetoothAdapter == null) {
+                    printInfo("No Bluetooth device, jurk");
+                }
+
+                if (!mBluetoothAdapter.isEnabled()) {
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                }
+
+                Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+                final BtArrayAdapter mArrayAdapter = new BtArrayAdapter(this, android.R.layout.simple_list_item_2, android.R.id.text2);
+
+                ListView deviceList = (ListView) findViewById(R.id.deviceList);
+                deviceList.setAdapter(mArrayAdapter);
+
+                deviceList.setOnItemClickListener(new OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view,
+                            int position, long id) {
+                        BluetoothDevice btd = (BluetoothDevice) parent.getItemAtPosition(position);
+
+                        String fileName = "";
+                        if (fileNameEditText.getText().toString().length() == 0) {
+                            fileName = UUID.randomUUID().toString().substring(0, 10);
+                        } else {
+                            fileName = fileNameEditText.getText().toString();
+                        }
+                        StorageUtils.StorageInfo storageDev = (StorageUtils.StorageInfo) storageListView.getSelectedItem();
+                        if (storageDev == null) {
+                            //wenn kein storage ausgewählt wurde, nehme einfach den ersten
+                            storageDev = (StorageUtils.StorageInfo) storageListView.getAdapter().getItem(0);
+                        }
+
+                        String filePath = storageDev.path + "/BluetoothLogger";
+                        
+                        
+
+                        LogWriter lw = new LogWriter(filePath,fileName, btd);
+                        logWriter.add(lw);
+                        lw.enableObserver((MainActivity) context);
+                        printInfo("started thread for logging "+btd.getName()+ " save in "+filePath);
+                        Thread thre = new Thread(lw);
+                        thre.start();
+                        
+                    }
+                });
+
+                // If there are paired devices
+                if (pairedDevices.size() > 0) {
+                    // Loop through paired devices
+                    for (BluetoothDevice device : pairedDevices) {
+                        // Add the name and address to an array adapter to show in a ListView
+                        mArrayAdapter.add(device);
+                    }
+                } else {
+                    printInfo("No paired bt-devices");
+                }
+
+                boolean startDiscovery = mBluetoothAdapter.startDiscovery();
+
+                // Create a BroadcastReceiver for ACTION_FOUND
+                final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+                    @Override
+                    public void onReceive(Context context, Intent intent) {
+                        String action = intent.getAction();
+                        // When discovery finds a device
+                        if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                            // Get the BluetoothDevice object from the Intent
+                            BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                            // Add the name and address to an array adapter to show in a ListView
+                            mArrayAdapter.add(device);
+                        }
+                    }
+                };
+                // Register the BroadcastReceiver
+                IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+                registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+        } else {
+            printInfo("External Storage Device wasn't found, buy a SD Card bitch");
+        }
+
+    }
+
+}
